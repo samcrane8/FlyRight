9f160975a96d4c5bd66fc0b81bf7c7957f2c47c3
Sam Crane <samcrane8@gmail.com>
2019-05-10 18:43:31 -0400

    removed old bash scripts

--- business-logic-server/install_on_redhat_enterprise_7.sh
+++ business-logic-server/install_on_redhat_enterprise_7.sh
@@ -10,12 +10,12 @@ psql -u postgres -d "flyright_db" -c "CREATE EXTENSION postgis;"
 psql -u postgres -d "flyright_db" -c "GRANT ALL PRIVILEGES on DATABASE flyright_db to flyright_user;"
 
 #setup nginx proxy?
-sudo ln /home/ubuntu/FlyRight/business-logic-server/flyright-nginx-site /etc/nginx/sites-enabled/
+sudo ln flyright-nginx-site /etc/nginx/sites-enabled/
 sudo service nginx restart
 
 # setup python
 sudo yum install -y python36 python36-pip
 # Setup virtualenvironment
 sudo yum install virtualenv
-virtualenv /home/ubuntu/FlyRight/business-logic-server/venv --python=python3.6
-/home/ubuntu/FlyRight/business-logic-server/venv/bin/pip install -r /home/ubuntu/FlyRight/business-logic-server/requirements.txt
+virtualenv venv --python=python3.6
+venv/bin/pip install -r requirements.txt
--- business-logic-server/push_prod.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rsync -avz /Users/samcrane/Documents/GitLab/Icarus/business-logic-server -e "ssh -i ../7305Key.pem" ubuntu@api.icarusmap.com:/home/ubuntu/ --exclude *.pyc --exclude venv/ --exclude dump.rdb --exclude secrets.json
