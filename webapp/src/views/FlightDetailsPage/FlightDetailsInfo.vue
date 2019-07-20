d3d945a1886cc1e42f1d135da336acd2eed26ffe
Tony Choi <tchoi52@gmail.com>
2019-07-20 12:46:37 -0400

    Finished preset clearance for FlightDetails

--- webapp/src/views/FlightDetailsPage/FlightDetailsInfo.vue
+++ webapp/src/views/FlightDetailsPage/FlightDetailsInfo.vue
@@ -252,34 +252,34 @@
         { text: 'Preset Message', value: '' },
 
         { text: 'CLEAR FLIGHT', 
-          value: "NAME, \n You're all set! The Georgia Tech Police Department Watch " +
+          value: this.user_info.user.first_name + ", \nYou're all set! The Georgia Tech Police Department Watch " +
           "Commanders and Dispatchers have been notified about your intended flight. " +
-          "\n Please note, filing a flight plan with the Georgia Tech Police Department " +
+          "\nPlease note, filing a flight plan with the Georgia Tech Police Department " +
           "does not alleviate you of the responsibility of adhering to all FAA regulations " + 
           "and safety recommendations.As always, if you experience any problems during " +
-          "your flight, please immediately call the GTPD at 404-894-2500. \n Fly safe!" },
+          "your flight, please immediately call the GTPD at 404-894-2500. \nFly safe!" },
 
         { text: 'ANOTHER FLIGHT', 
-          value: "NAME, \n Please exercise additional caution during your flight, as " +
+          value: this.user_info.user.first_name + ", \nPlease exercise additional caution during your flight, as " +
           "another UAS pilot has filed a flight plan with an overlapping time frame " +
-          "in the same location. \n Please note, filing a flight plan with the Georgia " +
+          "in the same location. \nPlease note, filing a flight plan with the Georgia " +
           "Tech Police Department does not alleviate you of the responsibility of adhering " +
           "to all FAA regulations and safety recommendations. In the unlikely event that " +
           "you experience any problems during your flight, please immediately call the " +
-          "GTPD at 404-894-2500. \n Fly safe!" },
+          "GTPD at 404-894-2500. \nFly safe!" },
 
         { text: 'CIVIC TWILIGHT WARNING', 
-          value: "NAME, \n Please exercise additional caution during your " +
+          value: this.user_info.user.first_name + ", \nPlease exercise additional caution during your " +
           "flight. You may not fly a small unmanned aircraft system before sunrise civil twilight, " +
           "nor after sunset civil twilight time. Civil twilight is defined as 30 minutes before " +
-          "sunrise and 30 minutes after sunset. \n Please note, filing a flight plan with the Georgia " +
+          "sunrise and 30 minutes after sunset. \nPlease note, filing a flight plan with the Georgia " +
           "Tech Police Department does not alleviate you of the responsibility of adhering " +
           "to all FAA regulations and safety recommendations. In the unlikely event " +
           "that you experience any problems during your flight, please immediately " +
-          "call the GTPD at 404-894-2500. \n Fly safe!" },
+          "call the GTPD at 404-894-2500. \nFly safe!" },
 
         { text: 'NIGHT FLYING', 
-          value: "NAME, \n It is NOT advised to fly at the current time, " +
+          value: this.user_info.user.first_name + ", \nIt is NOT advised to fly at the current time, " +
           "as the proposed time of your flight at night. The FAA prohibits the operation of " +
           "small unmanned aircraft systems at night without either a Certificate of " +
           "Authorization or Waiver. Filing a flight plan with the Georgia Tech Police " +
@@ -330,7 +330,10 @@
         if(this.selected) {
           //if there's a preset message selected
           this.clearance.message = this.selected;
+        } else {
+          this.clearance.message = this.flight.clearance.message;
         }
+        console.log(this.clearance.message);
         this.$emit('update_clearance', this.clearance);
       }
     },
